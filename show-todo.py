8f1b39abc8eb064876e944a181f0dbbf4df7af28
Dani <danipozo@correo.ugr.es>
2017-04-09 14:05:16 +0200

    Add simple argument parser

--- show-todo.py
+++ show-todo.py
@@ -2,11 +2,6 @@ import os, glob, argparse, sys
 
 # files = [f for f in glob.glob('./*') if os.path.isfile(f)]
 
-config_file = ".rules"
-
-def change_config_file(s):
-    config_file = s
-
 parser = argparse.ArgumentParser(description='Show FIXMEs, TODOs or whatever present in a project')
 parser.add_argument('-f', '--file', type=str, help='Specify rule file. .rules by default.')
 
