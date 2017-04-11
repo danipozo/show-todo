64369b2d183147be9c212df06bd0f23cc256a812
Dani <danipozo@correo.ugr.es>
2017-04-12 01:30:59 +0200

    Look in subdirectories and add initial support for LaTeX formatting

--- config.py
+++ config.py
@@ -1,12 +1,26 @@
-class ShowTodoConfig:
-    # Colors. Add more as needed.
+# Colors. Add more as needed.
+class TermColors:
     RED = "\033[91m"
     YELLOW = "\033[93m"
     PURPLE = "\033[95m"
 
-    # Regular expressions along with colors.
-    expressions = {
-    "FIXME": RED,
-    "WARNING": YELLOW,
-    "TODO": PURPLE
-    }
+    END_DELIMITER = "\033[0m"
+
+class LatexColors:
+    RED = "\\textcolor{red}{"
+    YELLOW = "\\textcolor{yellow}{"
+    PURPLE = "\\textcolor{purple}{"
+
+    END_DELIMITER = "}"
+    
+
+class ShowTodoConfig:
+
+    def __init__(self, colors):
+        self.colors = colors
+        # Regular expressions along with colors.
+        self.expressions = {
+        "FIXME": self.colors.RED,
+        "WARNING": self.colors.YELLOW,
+        "TODO": self.colors.PURPLE
+        }
--- show-todo.py
+++ show-todo.py
@@ -1,22 +1,47 @@
-import os, glob, argparse, sys, subprocess, re
+import os, glob, argparse, sys, subprocess, re, fnmatch
 import config
 
-conf = config.ShowTodoConfig()
 
 def insert (source_str, insert_str, pos):
     return source_str[:pos]+insert_str+source_str[pos:]
 
+# Insert two delimiters around a substring in the given string s.
+def insert_color_mark(delimiter_start, delimiter_end, s, pos_start, pos_end):
+    s = insert(s, delimiter_start, pos_start)
+    s = insert(s, delimiter_end, pos_end+len(delimiter_start))
+
+    return s
+
+# Build argument parser
 parser = argparse.ArgumentParser(description='Show FIXMEs, TODOs or whatever present in a project')
 parser.add_argument('-f', '--file', type=str, default='.rules', help='Specify rule file. .rules by default.')
+parser.add_argument('--output-format', type=str, default='term', choices=['term', 'latex'])
 
-
+# Parse arguments and decide which color format to apply
 args = vars(parser.parse_args())
+colors = config.TermColors
 
+if args['output_format'] == 'latex':
+    colors = config.LatexColors
+
+conf = config.ShowTodoConfig(colors)
+
+# List files matching the regular expressions in the file specified with -f
 files = []
+for dp, dn, filenames in os.walk('.'):
+    for ff in filenames:
+        files.append(os.path.join(dp, ff))
+
 with open(args['file'], 'r') as rules_f:
     for line in rules_f:
-        files += [f for f in glob.glob('./'+line.strip()) if os.path.isfile(f)]
+        files += fnmatch.filter(files, line)
 
+
+# for dp, dn, filenames in os.walk(path):
+#     for ff in filenames:
+#         if fnmatch.fnmatch(ff, 'My patterns here'):
+#              list.append(os.path.join(dp, ff))
+        
 exprs = [(re.compile(expr), conf.expressions[expr]) for expr in conf.expressions]
 for f in files:
     with open(f, 'r') as file:
@@ -29,9 +54,10 @@ for f in files:
 
                 for m in ms:
                     match = True
-                    line = insert(line,c,m.start())
-                    line = insert(line,'\033[0m', m.end()+5)
-
+                    # line = insert(line,c,m.start())
+                    # line = insert(line,'\033[0m', m.end()+5)
+                    line = insert_color_mark(c, conf.colors.END_DELIMITER, line, m.start(), m.end())
+                    
             if match == True:
                 print("{0}:{1} {2}".format(f,i,line.rstrip()))
 
